f882e1906ec9173576992c878809c5c93b57b18c
Jeremy Deane <jeremy.deane@gmail.com>
2015-11-01 13:10:20 -0500

    Fixed error with parsing JSON

--- src/main/java/cogito/online/messaging/OrderProcessingMessageListener.java
+++ src/main/java/cogito/online/messaging/OrderProcessingMessageListener.java
@@ -1,18 +1,11 @@
 package cogito.online.messaging;
 
-import java.io.InputStream;
-
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import cogito.online.model.Orders;
 
 /**
  * Listens for Magic Supply Orders
@@ -21,51 +14,20 @@ import cogito.online.model.Orders;
 public class OrderProcessingMessageListener implements MessageListener {
 	
 	private static final Logger logger = LoggerFactory.getLogger
-			(OrderProcessingMessageListener.class);
-	
-	private final JAXBContext jaxbContext;
-	
-	/**
-	 * Default Constructor
-	 * @throws Exception
-	 */
-	public OrderProcessingMessageListener () throws Exception {
-		jaxbContext = JAXBContext.newInstance(Orders.class);
-	}
+			(OrderProcessingMessageListener.class);	
 
 	@Override
 	public void onMessage (Message message) {
-		
-		Orders orders = null;
 
 		try {			
 			
 			TextMessage textMessage = (TextMessage) message;
-				
-			orders = xmlToPOJO(textMessage.getText());
 			
-			logger.debug("Processed Magic Order:" + orders);
+			logger.debug("Processed Magic Order: \n" + textMessage.getText());
 			
 		} catch (Exception e) {
 			
 			throw new RuntimeException(e);
 		}
-	}
-	
-	/**
-	 * Transrept to POJO from xml
-	 * @param xml
-	 * @return Orders
-	 * @throws Exception
-	 */
-	private Orders xmlToPOJO (String xml) throws Exception {
-		
-		logger.debug("Orders Payload: " + xml);
-	
-		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-		InputStream stream = IOUtils.toInputStream (xml);	
-
-		return (Orders) unmarshaller.unmarshal(stream);		
 	}
 }
--- src/main/java/cogito/online/messaging/SingleOrderProcessingMessageListener.java
+++ src/main/java/cogito/online/messaging/SingleOrderProcessingMessageListener.java
@@ -1,20 +1,11 @@
 package cogito.online.messaging;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import cogito.online.model.Order;
 
 /**
  * Listens for Magic Supply Order (Single)
@@ -24,53 +15,19 @@ public class SingleOrderProcessingMessageListener implements MessageListener {
 	
 	private static final Logger logger = LoggerFactory.getLogger
 			(SingleOrderProcessingMessageListener.class);
-	
-	private final JAXBContext jaxbContext;
-	
-	/**
-	 * Default Constructor
-	 * @throws Exception
-	 */
-	public SingleOrderProcessingMessageListener () throws Exception {
-		jaxbContext = JAXBContext.newInstance(Order.class);
-	}
 
 	@Override
 	public void onMessage (Message message) {
-		
-		Order order = null;
 
 		try {			
 			
 			TextMessage textMessage = (TextMessage) message;
-				
-			order = xmlToPOJO(textMessage.getText());
 			
-			List<Order> orderList = new ArrayList<Order>();
-			orderList.add(order);
-			
-			logger.debug("Processed Magic Order:" + order);
+			logger.debug("Processed Magic Order: \n" + textMessage.getText());
 			
 		} catch (Exception e) {
 			
 			throw new RuntimeException(e);
 		}
-	}
-	
-	/**
-	 * Transrept to POJO from xml
-	 * @param xml
-	 * @return Orders
-	 * @throws Exception
-	 */
-	private Order xmlToPOJO (String xml) throws Exception {
-		
-		logger.debug("Order : " + xml);
-	
-		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-		InputStream stream = IOUtils.toInputStream (xml);	
-
-		return (Order) unmarshaller.unmarshal(stream);		
 	}
 }
